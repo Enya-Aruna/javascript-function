41c1688f56b2f9f7eaf78106c0f924c77ce5104c
enya <salmaislam489265@gmail.com>
2023-12-10 16:32:22 +0600

    squre

--- squre.js
+++ squre.js
@@ -1,8 +1,7 @@
-var n = 2;
+
 function squre(num){
-    var ans = num*num ;
+    var ans =Math.pow(num , 2) ;
     return ans ;
 }
-var squre2 = squre(n);
-var squre4 = squre(4);
-console.log(squre2 , squre4)
+
+console.log(squre(3))
